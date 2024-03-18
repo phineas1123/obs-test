cd6c88449be95d6c3c3febcd65059aeba64c0bb3
phineas1123 <muhammadimamt@gmail.com>
2024-03-18 20:13:26 +0700

    bugfix count stock if data empty (withdraw empty)

--- src/main/java/com/example/obstest/controller/InventoryController.java
+++ src/main/java/com/example/obstest/controller/InventoryController.java
@@ -1,24 +1,59 @@
 package com.example.obstest.controller;
 
+import com.example.obstest.dto.InventoryDto;
+import com.example.obstest.exception.ValidationException;
 import com.example.obstest.model.Inventory;
 import com.example.obstest.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
     @Autowired
-    InventoryService inventoryService;
+    private InventoryService inventoryService;
 
     @GetMapping("/all")
-    public Page<Inventory> getInventories(@RequestParam(defaultValue = "0") int page,
-                               @RequestParam(defaultValue = "10") int size) {
+    public Page<Inventory> getInventorys(@RequestParam(defaultValue = "0") int page,
+                                 @RequestParam(defaultValue = "10") int size) {
         return inventoryService.getInventoryWithPagination(page, size);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Inventory> getInventoryById(@PathVariable Long id) throws ValidationException {
+        Inventory inventory = inventoryService.getInventoryById(id);
+        return ResponseEntity.ok(inventory);
+    }
+
+    @PostMapping
+    public ResponseEntity<Inventory> createInventory(@RequestBody InventoryDto dto) {
+        Inventory savedInventory = inventoryService.saveInventory(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedInventory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Inventory> updateInventory(@PathVariable Long id, @RequestBody InventoryDto dto)  throws  ValidationException{
+        Inventory updatedInventory = inventoryService.patchInventory(id, dto);
+        return ResponseEntity.ok(updatedInventory);
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String, String> deleteInventory(@PathVariable Long id) {
+        inventoryService.deleteInventory(id);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "DELETED");
+        return response;
+    }
+
+    @GetMapping("/stock/{itemId}")
+    public ResponseEntity<Long> getTotalStock(@PathVariable Long itemId) throws ValidationException {
+        Long totalStock = inventoryService.getTotalStock(itemId);
+        return ResponseEntity.ok(totalStock);
+    }
 }
--- src/main/java/com/example/obstest/repository/InventoryRepository.java
+++ src/main/java/com/example/obstest/repository/InventoryRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    @Query(value = "SELECT SUM(qty) FROM INVENTORY WHERE ITEM_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT COALESCE(SUM(qty), 0) FROM inventories WHERE ITEM_ID = ?1 AND TYPE = 'W' ", nativeQuery = true)
     Long countWithdrawStock(Long itemId);
 
-    @Query(value = "SELECT SUM(qty) FROM INVENTORY WHERE ITEM_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT COALESCE(SUM(qty), 0) FROM inventories WHERE ITEM_ID = ?1 AND TYPE = 'T' ", nativeQuery = true)
     Long countTopupStock(Long itemId);
 }
